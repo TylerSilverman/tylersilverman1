f5484f1a508ef5ab4db80eae3acde1f1667bfbd1
Tyler Silverman <silverman.tyler@gmail.com>
2020-11-12 18:20:05 -0500

    touched up the script file adding a variable

--- index.js
+++ index.js
@@ -1,7 +1,37 @@
+var date = "#date"
+
+
 $(document).ready(function(){
     
     var timeNow = moment().format('MMMM Do YYYY, h:mm:ss a');
     $("#date").text(timeNow);
-    
+    console.log(timeNow)
+
     var timeNow = moment().hour();
-    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
