ff170d1e9e60c0832dad15788fcfb40b5682c55f
Tyler Silverman <silverman.tyler@gmail.com>
2021-11-30 12:50:25 -0500

    sadsf

--- client/src/index.js
+++ client/src/index.js
@@ -14,6 +14,6 @@ render(
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// or send to an analytics endpoint.
 // reportWebVitals();
 reportWebVitals(console.log)
